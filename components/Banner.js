b769651095eaba19daef9e0f10607ce1cf487a08
kimearwud22 <kimearwud22@gmail.com>
2023-08-09 14:55:42 -0700

    integrate banner and visimisi

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,30 @@
 import { Carousel, Typography } from "@material-tailwind/react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Banner() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getBanner = () => {
+    fetch("/api/banner/all", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getBanner();
+  }, []);
+
   return (
     <Carousel
       className="rounded-0"
@@ -21,31 +44,47 @@ export default function Banner() {
       )}
     >
       <div className="relative h-full w-full">
+      {data.length > 0 ? (
+          data.map((item, index) => (
+        <div key={index}>
         <Image
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-          alt="image 1"
-          className="w-full h-[30rem] object-cover"
-          width={1920}
-          height={1080}
-        />
-        <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
-          <div className="w-3/4 text-center md:w-2/4">
-            <Typography
-              variant="h1"
-              color="white"
-              className="mb-4 text-lg"
-            >
-              BADAN EKSEKUTIF MAHASISWA
-            </Typography>
-            <Typography
-              className="mb-12 opacity-80 text-2xl font-bold text-light"
-            >
-             SEKOLAH TINGGI ILMU KOMPUTER PGRI BANYUWANGI
-            </Typography>
-          </div>
-        </div>
+              src={item.image}
+              alt="image 1"
+              className="w-full h-[30rem] object-cover"
+              width={1920}
+              height={1080} />
+              <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/55">
+                <div className="w-3/4 text-center md:w-2/4">
+                  <Typography
+                    variant="h1"
+                    color="white"
+                    className="mb-4 text-lg"
+                  >
+                    BADAN EKSEKUTIF MAHASISWA
+                  </Typography>
+                  <Typography
+                    className="mb-12 opacity-80 text-2xl font-bold text-light"
+                  >
+                    SEKOLAH TINGGI ILMU KOMPUTER PGRI BANYUWANGI
+                  </Typography>
+                </div>
+              </div>
+              </div>
+          ))
+        ) : (
+          <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+            <div className="w-3/4 text-center md:w-2/4">
+              <Typography variant="h1" color="white" className="mb-4 text-lg">
+                BADAN EKSEKUTIF MAHASISWA
+              </Typography>
+              <Typography className="mb-12 opacity-80 text-2xl font-bold text-light">
+                SEKOLAH TINGGI ILMU KOMPUTER PGRI BANYUWANGI
+              </Typography>
+            </div>
+            </div>
+        )}
       </div>
-      <Image
+      {/* <Image
         src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
         alt="image 2"
         className="w-full h-[30rem] object-cover"
@@ -58,7 +97,7 @@ export default function Banner() {
         className="w-full h-[30rem] object-cover"
         width={1920}
         height={1080}
-      />
+      /> */}
     </Carousel>
   );
 }
--- components/VisiMisi.js
+++ components/VisiMisi.js
@@ -1,15 +1,41 @@
 import React from "react";
 import Image from "next/image";
 import { Typography, List, ListItem } from "@material-tailwind/react";
+import { useEffect, useState } from "react";
 
 export default function VisiMisi() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getKabinet = () => {
+    fetch("/api/kabinet/all", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getKabinet();
+  }, []);
+
   return (
+    <>
+    {data.length > 0 ? (
+          data.map((item, index) => (
     <div className="flex flex-col-reverse md:flex-row px-3 py-6 items-center justify-center bg-light">
       <div className="bg-blue-200 p-4 m-3 text-center ">
         <Typography className="text-dark font-semibold">Visi</Typography>
         <List>
           <ListItem className="py-1.5 px-3 text-sm font-normal text-blue-gray-900 hover:bg-transparent">
-            Menjunjung tinggi martabat mahasiswa
+            {item.visi}
           </ListItem>
           <ListItem className="py-1.5 px-3 text-sm font-normal text-blue-gray-900 hover:bg-transparent">
             Trash
@@ -23,7 +49,7 @@ export default function VisiMisi() {
         <Typography className="text-dark font-semibold">Misi</Typography>
         <List>
           <ListItem className="py-1.5 px-3 text-sm font-normal text-blue-gray-900 hover:bg-transparent">
-            Menjadikan mahasiswa menjadi penguasa kampus
+            {item.misi}
           </ListItem>
           <ListItem className="py-1.5 px-3 text-sm font-normal text-blue-gray-900 hover:bg-transparent">
             Trash
@@ -37,12 +63,18 @@ export default function VisiMisi() {
         <Typography className="text-dark text-xl font-semibold mb-3">Kabinet</Typography>
         <Image
           className="max-w-full h-auto"
-          src="/bem.png"
+          src={item.image}
           alt="Logo Kabinet"
           width={300}
           height={300}
         />
       </div>
     </div>
+    ))) : (
+      <div className="flex flex-col-reverse md:flex-row px-3 py-6 items-center justify-center bg-light">
+        <h1 className="text-2xl font-semibold text-center">Loading...</h1>
+      </div>
+    )}
+    </>
   );
 }
